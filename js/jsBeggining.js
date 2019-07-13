03b36feff61b94977adc1ff09cd7c012feb45caa
Andrzej Kulesza <andrzejkulesza6@gmail.com>
2019-07-13 20:16:31 +0200

    Adding clickable

--- js/jsBeggining.js
+++ js/jsBeggining.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-    
+    // $("nav div").css("color", "purple");
+    // $("nav div").click(function(e){
+    //     $(this).toggleClass("selected");
+    // });
+    $(".clickable").click(function(e){
+        $(this).toggleClass("selected");
+    });
 });
